6866535f94132e66016b8bf368b5b9d580745cf4
Dean Alif Ahmad <deanalifahmad49@gmail.com>
2023-11-21 12:36:18 +0700

    feat: add react-slick and slick-carousel

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,5 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
